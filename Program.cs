383cdafc19aeab4a78d72065c8964a3257061eff
Greg Smulko <Greg.Smulko@red-gate.com>
2023-04-17 12:56:43 +0100

    Initial commit - from the Worker template

---
+++ Program.cs
@@ -0,0 +1,7 @@
+using WorkerServiceSandbox;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+    .Build();
+
+await host.RunAsync();
